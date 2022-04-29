cb0138295ef5efb82a3905abe792f397ac9ed6f2
andreev-alef <andreev.alef@gmail.com>
2022-04-29 14:50:26 +0300

    Добавлен bootstrap

---
+++ git.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        git add -A & git commit -m "Добавлен bootstrap" & git push
+    </body>
+</html>
